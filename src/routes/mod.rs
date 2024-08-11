1ee52a1b09652be6c60c92b82a2ec173442bf8c8
artemijfridriksen <freshart666@gmail.com>
2024-08-10 20:26:51 -0400

    auth refactoring

--- src/routes/mod.rs
+++ src/routes/mod.rs
@@ -1,13 +1,17 @@
 use std::sync::Arc;
 
+use auth::auth_user_routes;
 use axum::Router;
 use migration::sea_orm::DatabaseConnection;
 
 pub mod user;
 pub mod blog;
+pub mod auth;
 
 pub fn create_all_routes(db: Arc<DatabaseConnection>) -> Router {
     Router::new()
+        .merge(auth_user_routes(db.clone()))
         .merge(user::user_routes(db.clone()))
         .merge(blog::blog_routes(db))
+        
 }
